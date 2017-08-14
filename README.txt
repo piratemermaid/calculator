00e52789d854aed2f8f5782d8ab27a5a3dcfd3c5
piratemermaid <schnur.ariel@gmail.com>
2017-08-14 19:07:43 -0400

    Added UI functionality

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
 This is a calculator.
+
+Done:
+-Display what's been entered on the UI
+-Set the UI back to 0 when the Clear button is clicked
+
+To Do:
+-Calculate the value of what's been entered
+-Make sure a number follows an operator entry
+-Create error check for dividing by 0
--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log('App started');
+
+var entries = [];
+
+$('button').click(function() {
+  entries.push(this.value);
+  entryString = entries.toString();
+  entryString = entryString.replace(',', '')
+  document.getElementById('answer').textContent = entries.join('');
+})
+
+$('#clear').click(function() {
+  entries = [];
+  document.getElementById('answer').textContent = '0';
+})
+
+$('#equals').click(function() {
+  document.getElementById('answer').textContent = 'answer';
+})
