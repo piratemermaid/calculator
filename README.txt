e85ef88daa8ad2b67a05c4b789a065bc2e9ba02f
piratemermaid <schnur.ariel@gmail.com>
2017-08-15 10:26:03 -0400

    Added error checks

--- README.txt
+++ README.txt
@@ -5,7 +5,6 @@ Done:
 -Set the UI back to 0 when the Clear button is clicked
 -Calculate the value of what's been entered
 -Clear field when new button is pressed after =
-
-To Do:
--Make sure a number follows an operator entry
+-Make sure a number follows an operator entry, unless it is a -
+-Make sure - can only be entered if entry before previous entry is a number
 -Create error check for dividing by 0
--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 console.log('App started');
 
-var entries = [];
+var entries = [], answer;
 
 $('button').click(function() {
-  entries.push(this.value);
-  document.getElementById('answer').textContent = entries.join('');
+  if(entries.length === 0 && !isNaN(this.value)) {
+    enterValue(this.value);
+  }
+  else if(!(isNaN(this.value) && isNaN(entries[entries.length - 1]))) {
+    enterValue(this.value);
+  }
+  else if(entries.length > 0 && this.value === '-' && !isNaN(entries[entries.length - 2])) {
+    enterValue(this.value);
+  }
 })
 
 $('#clear').click(function() {
   entries = [];
-  document.getElementById('answer').textContent = '0';
 })
 
 $('#equals').click(function() {
   var stringEntries = document.getElementById('answer').textContent;
-  console.log(eval(stringEntries));
-  document.getElementById('answer').textContent = eval(stringEntries);
+  stringEntries = stringEntries.replace('--', '+');
+  answer = eval(stringEntries);
+  console.log(answer);
+  if(answer === Infinity || answer === -Infinity) {
+    document.getElementById('answer').textContent = 'Cannot divide by 0';
+  }
+  else {
+    document.getElementById('answer').textContent = answer;
+  }
   entries = [];
 })
+
+function enterValue(val) {
+  entries.push(val);
+  document.getElementById('answer').textContent = entries.join('');
+}
